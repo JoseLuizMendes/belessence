2081fb7da29e2cadbe974947ef38aed9f81f4082
José Luiz Mendes <braabozeca@gmail.com>
2025-11-21 18:30:52 +0000

    refactor: Remover imports não utilizados e otimizar o código da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
 "use client";
 
-import { motion } from "framer-motion";
-import {
-  Star,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
 import Footer from "@/components/footer";
 import Newsletter from "@/components/newsletter";
-import { fadeInUp } from "@/components/ui/fadeInUp";
-import { staggerContainer } from "@/components/ui/staggerContainer";
 import Features from "@/components/features";
 import Header from "@/components/header";
-import { useCart } from "@/components/cart";
 import Hero from "@/components/hero";
 import CollectionsProducts from "@/components/collections-products";
 import FeatureProducts from "@/components/feature-products";
 
 
 export default function Home() {
-  const { addToCart } = useCart();
 
 
   return (
